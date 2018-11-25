ee7333cabe29687c40e1359fcc0b25b43c8a8f9f
Youssef <youssef.n6@gmail.com>
2018-11-25 17:09:47 +0100

    player movement script
    done

--- Random game/Assets/Scripts/PlayerMovement.cs
+++ Random game/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-}
+    private Rigidbody2D myBody;
+    private float moveForce = 10f;
+
+
+    // Use this for initialization
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        myBody.velocity = new Vector3(h * moveForce, 0f, 0f);
+    }
+} //class
+
