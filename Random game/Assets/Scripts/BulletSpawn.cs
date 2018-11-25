95ad3425fde324cf374b5e1aa862e7e66b2f0a99
Youssef <youssef.n6@gmail.com>
2018-11-25 17:51:25 +0100

    bulletspawn script

--- Random game/Assets/Scripts/BulletSpawn.cs
+++ Random game/Assets/Scripts/BulletSpawn.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Shoot : MonoBehaviour {
+public class BulletSpawn : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
